cc57f643fc80d5c1296ec837f2a1efe8a48c1ef1
AnggaWikaNugraha <anggawika18@gmail.com>
2021-06-14 20:12:53 +0700

    manage cors

--- bookstore-server/routes/api.php
+++ bookstore-server/routes/api.php
@@ -15,5 +15,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
+    
     return $request->user();
+
 });
+
+Route::middleware(['cors'])->group(function () {
+    
+    Route::get('buku/{judul}', 'BookController@cetak');
+
+});
+   
+   
